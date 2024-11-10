d9f36d47ca94e5c240211f648bd825225a006a2e
NekoCyan <cyanhijirikawa88@gmail.com>
2024-11-11 01:23:25 +0700

    feat: include admin on header.

--- src/components/header/headerComponents/headerMiscs/HeaderAccount.tsx
+++ src/components/header/headerComponents/headerMiscs/HeaderAccount.tsx
@@ -1,24 +1,38 @@
 'use client';
 
-import { ROUTES } from '@/utils';
+import { isAdmin, ROUTES } from '@/utils';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { Fragment } from 'react';
 
 export default function HeaderAccount() {
-	const { status } = useSession();
+	const { data, status } = useSession();
 	const isAuthenticated = status === 'authenticated';
 
 	return (
-		<Link
-			href={isAuthenticated ? ROUTES.Profile : ROUTES.Auth}
-			className='text-center text-gray-700 hover:text-primary transition relative'
-		>
-			<div className='text-2xl'>
-				<i className='fa-regular fa-user'></i>
-			</div>
-			<div className='text-xs leading-3'>
-				{isAuthenticated ? 'Profile' : 'Auth'}
-			</div>
-		</Link>
+		<Fragment>
+			<Link
+				href={isAuthenticated ? ROUTES.Profile : ROUTES.Auth}
+				className='text-center text-gray-700 hover:text-primary transition relative'
+			>
+				<div className='text-2xl'>
+					<i className='fa-regular fa-user'></i>
+				</div>
+				<div className='text-xs leading-3'>
+					{isAuthenticated ? 'Profile' : 'Auth'}
+				</div>
+			</Link>
+			{isAdmin(data) && (
+				<Link
+					href={ROUTES.Admin}
+					className='text-center text-gray-700 hover:text-primary transition relative'
+				>
+					<div className='text-2xl'>
+						<i className='fa-regular fa-user'></i>
+					</div>
+					<div className='text-xs leading-3'>Admin</div>
+				</Link>
+			)}
+		</Fragment>
 	);
 }
