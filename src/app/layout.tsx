d7d02b3debacb75f7d7f2fe6ac5ae6a071214859
NekoCyan <cyanhijirikawa88@gmail.com>
2024-11-24 08:45:05 +0700

    feat: js & dependencies.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,8 @@ export default async function RootLayout({
 				<link rel='stylesheet' href='/assets/css/main.css' />
 
 				<script src='/assets/js/jquery.min.js'></script>
+				<script src='/assets/js/jquery.zoom.min.js'></script>
+				<script src='/assets/js/nouislider.min.js'></script>
 				<script src='/assets/js/bootstrap.min.js'></script>
 				<script src='/assets/js/main.js'></script>
 			</head>
